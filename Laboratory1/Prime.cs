d7128b0c7d30f4aa891953e5b8bf032d9d99ed19
RasterDX <rasterplane@hotmail.com>
2020-03-23 09:46:04 -0700

    Add project files.

---
+++ Laboratory1/Prime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Laboratory1
+{
+    public static class Prime
+    {
+        public static bool IsPrime(int num)
+        {
+            for (var i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        
+        public static async Task<int> MethodOne(int num)
+        {
+            for (var i = num; i >= 2; i--)
+            {
+                if (IsPrime(i))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static async Task<int> MethodTwo(int num)
+        {
+            var foundPrime = -1;
+            for (var i = 2; i < num; i++)
+            {
+                if (IsPrime(i))
+                {
+                    foundPrime = i;
+                }
+            }
+
+            return foundPrime;
+        }
+    }
+}
---
+++ Laboratory1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Laboratory1
+{
+    internal class Program
+    {
+        
+        public static String GetTimestamp(DateTime value)
+        {
+            return value.ToString("HH::mms::s::ffff");
+        }
+        
+        public static void Main(string[] args)
+        {
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            var list = new List<string>();
+
+            var threadOne = new Thread(async delegate () {
+                list.Add($"Start Fir: FirstMethod {GetTimestamp(DateTime.Now)} Numar natural dat: {num}");
+                var result = await Prime.MethodOne(num);
+                list.Add($"End Fir: FirstMethod {GetTimestamp(DateTime.Now)} Numar prim dat: {result}");
+            });
+            var threadTwo = new Thread(async delegate () {
+                list.Add($"Start Fir: SecondMethod {GetTimestamp(DateTime.Now)} Numar natural dat: {num}");
+                var result = await Prime.MethodTwo(num);
+                list.Add($"End Fir: SecondMethod {GetTimestamp(DateTime.Now)} Numar prim dat: {result}");
+            });
+            threadOne.Name = "FirstMethod";
+            threadTwo.Name = "SecondMethod";
+            String timeStamp = GetTimestamp(DateTime.Now);
+            threadOne.Start();
+            threadTwo.Start();
+            // System.Threading.Thread.Sleep(500);
+            threadOne.Join();
+            threadTwo.Join();
+            foreach (var entry in list)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+}
